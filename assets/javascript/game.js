04b84760bb538341ca2f635d42d72b9ec30feec5
JC Scalabre <jc_scalabre@hotmail.com>
2017-09-26 15:43:15 -0500

    tuesday evening commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,12 @@
 // Global Variables: hp / attack power / enemy hp / counter attack power.
 
-//Link's Stats:
-var hp = 200;
-var ap = 25;
-var ehp = 120;
-var cap = 20;
+var attackpower;
+var baseattackpower;
 
-var linkstats = { "hp": 130, "ap": 20, "cap": 10 }
-var peachstats = { "hp": 100, "ap": 10, "cap": 15 }
-var yoshistats = { "hp": 80, "ap": 15, "cap": 20 }
-var bowserstats = { "hp": 150, "ap": 20, "cap": 15 }
+var linkstats = { "hp": 130, "ap": 7, "cap": 10 }
+var peachstats = { "hp": 100, "ap": 8, "cap": 12 }
+var yoshistats = { "hp": 80, "ap": 9, "cap": 14 }
+var bowserstats = { "hp": 150, "ap": 10, "cap": 13 }
 
 var info = "Welcome Super Smash Bros jQuery! Choose a character to begin."
 
@@ -28,6 +25,9 @@ $("#char1").on("click", function() {
 		console.log("You chose Link.");
 		$("#hp").text(linkstats.hp);
 		$("#ap").text(linkstats.ap);
+		attackpower = linkstats.ap;
+		baseattackpower = attackpower;
+		console.log("Link's AP: " + attackpower);
 		$("#info").text("You chose Link! Now pick the enemy you would like to target.");
 		$("#yourhp").css("visibility", "visible");
 		$("#yourap").css("visibility", "visible");
@@ -92,6 +92,9 @@ $("#char2").on("click", function() {
 		console.log(lockstate);
 		$("#hp").text(peachstats.hp);
 		$("#ap").text(peachstats.ap);
+		attackpower = peachstats.ap;
+		baseattackpower = attackpower;
+		console.log("Peach's AP: " + attackpower);
 		$("#info").text("You chose Peach! Now pick the enemy you would like to target.");
 		$("#yourhp").css("visibility", "visible");
 		$("#yourap").css("visibility", "visible");
@@ -149,6 +152,9 @@ $("#char3").on("click", function() {
 		console.log("You chose Yoshi.");
 		$("#hp").text(yoshistats.hp);
 		$("#ap").text(yoshistats.ap);	
+		attackpower = yoshistats.ap;
+		baseattackpower = attackpower;
+		console.log("Yoshi's AP: " + attackpower);
 		$("#info").text("You chose Yoshi! Now pick the enemy you would like to target.");
 		$("#yourhp").css("visibility", "visible");
 		$("#yourap").css("visibility", "visible");
@@ -207,6 +213,9 @@ $("#char4").on("click", function() {
 		console.log("You chose Bowser.");
 		$("#hp").text(bowserstats.hp);
 		$("#ap").text(bowserstats.ap);
+		attackpower = bowserstats.ap;
+		baseattackpower = attackpower;
+		console.log("Bowser's AP: " + attackpower);
 		$("#info").text("You chose Bowser! Now pick the enemy you would like to target.");
 		$("#yourhp").css("visibility", "visible");
 		$("#yourap").css("visibility", "visible");
@@ -263,33 +272,48 @@ $("#char4").on("click", function() {
 // When the attack button is pressed:
 $("#attack").on("click", function() {
 	
+	var targetcap = document.getElementById("cap").innerHTML;
+
+	$("#info").html("You hit your target for " + attackpower + " damage. <br><br> Your target hit you for " + targetcap + " damage.")
+
 	// Code that reduces your HP:
 	var chosenhp = document.getElementById("hp").innerHTML;
-	console.log("Your HP: " + chosenhp);
-	var targetcap = document.getElementById("cap").innerHTML;
-	console.log("Target's CAP: " + targetcap);
+	// console.log("Your HP: " + chosenhp);
+	// console.log("Target's CAP: " + targetcap);
 	chosenhp = chosenhp - targetcap;
-	console.log("Your HP: " + chosenhp);
+	console.log("Your HP after being attacked: " + chosenhp);
 	$("#hp").text(chosenhp); 
 
 	// Code that reduces enemy HP:
 	var enemyhp = document.getElementById("ehp").innerHTML;
-	console.log("Enemy HP: " + enemyhp);
-	var attackpower = document.getElementById("ap").innerHTML;
 	enemyhp = enemyhp - attackpower;
 	$("#ehp").text(enemyhp);
+	console.log("Enemy HP: " + enemyhp);
 
+	attackpower = attackpower + baseattackpower;
 	console.log("Attack Power: " + attackpower);
+
+	$("#ap").html(attackpower);
 
 	// Win / Loss conditions:
 	if (chosenhp <= 0) {
-		$("#info").text("Your HP reached 0 and you died!");
+		$("#info").html("GAME OVER! <br> Your HP reached 0 and you died!");
 		console.log("You died.")
+		$("#attack").css("display", "none");
+		$("#reset").css("display", "inline");
 	}
 	if (enemyhp <= 0) {
-		$("#char2enemy").css("opacity", "0.3");
-		ehp = 200;
-		$("#info").text("You killed your target! Pick a new one.");
-		console.log("You killed your target.")
+		// $("#char2enemy").css("opacity", "0.3");
+		$("#info").html("You killed your target! <br> Pick a new one. <br> If all enemies are dead, congratulations, you have won!");
+		console.log("You killed your target.");
+		$("#attack").css("visibility", "hidden");
+		// $("#playagain").css("display", "inline");
 	}
 });
+
+function refresh() {
+	location.reload();
+}
+
+$("#reset").on("click", refresh);
+$("#playagain").on("click", refresh);
